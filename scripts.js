28084c9de38f44dc7f80448c15b506490e89712f
bailey <paulbaileyuni@gmail.com>
2023-11-05 19:41:58 -0800

    Added Javascript Code

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 /* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('subscription-form');
+  const emailInput = document.getElementById('email');
+  const messageArea = document.querySelector('.message');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const email = emailInput.value.trim();
+
+    if (email === '') {
+      messageArea.textContent = 'Please enter a valid email address.';
+    } else {
+      messageArea.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+    }
+
+    emailInput.value = '';
+  });
+});
+
